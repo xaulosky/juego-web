368d1f1d3a6c4e9f27144f89bf6e0f58c8593449
xaulosky <pascifuentes@gmail.com>
2019-07-30 12:26:40 -0400

    inicio-juego-web

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Juego web</title>
+</head>
+<body>
+    <h1>Hola que tal</h1>
+    <script src="js/inicio.js"></script>
+</body>
+</html>
---
+++ js/inicio.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function(){
+console.log("Hola que tal");
+}),false;
+//DOM  = document  object model
